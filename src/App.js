cadc29b696ff65e5f1057ebfcb31f601b45afa76
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-10-05 21:36:13 +0530

    Created components and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchedFeed,
+} from "./components";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/video/:searchedTerm' element={<SearchedFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
